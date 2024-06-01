9ce2101e2b4b8dac3933874bded3bf9b988e319e
Dante Kelly <dante@dantekelly.com>
2024-05-31 21:09:22 -0600

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -19,9 +19,25 @@ func main() {
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	c, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	fmt.Println("Connection accepted")
+	response := formatResponse(Status{200, "OK"})
+	c.Write([]byte(response))
+}
+
+type Status struct {
+	status int
+	reason string
+}
+
+func formatResponse(status Status) string {
+	return fmt.Sprintf("HTTP/1.1 %d %s\r\n\r\n", status.status, status.reason)
+	// fmt.Printf("Content-Length: %d\n", len(body))
+	// fmt.Printf("Content-Type: text/plain\n\n")
+	// fmt.Println(body)
 }
